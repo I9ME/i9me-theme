cec6bdfe5976f8b23a76a828f2c2a46f8846f134
DIOGO SOARES <diogo@i9me.com.br>
2017-11-23 13:37:31 -0300

    novos padroes de espaçamentos / caminho dinamico do video / tempo de animação no servicos / lopp

--- templates/servico-loop.php
+++ templates/servico-loop.php
@@ -11,11 +11,11 @@ $term_web = get_term_by('name', 'web', 'tipo-de-servico');
  ?>
  	<article class="servicos__design" id="design">
 		<div class="boxes">
-			<div class="container">
+			<div class="container-full">
 
 				<div class="boxes__box-left">
 
-					<header class="header-section header-section--area-design">
+					<header class="header-section header-section--area-design left-padding--interCol">
 						<div class="header-section__box header-section__box--design">
 							<span class="header-section__title header-section__title--design">
 								<i class="icon-design"></i>
@@ -42,7 +42,7 @@ $term_web = get_term_by('name', 'web', 'tipo-de-servico');
 
 				<div class="boxes__box-right">
 
-					<ul class="lista lista--design">
+					<ul class="lista lista--design right-padding--interCol">
 						<?php
 							$tax_slug =  'tipo-de-servico';
 			    			$args = array( 'post_type' => 'servico', 'posts_per_page' => $limit_res, 'order' => 'ASC', 'tax_query' => array(  array( 'taxonomy'  => $tax_slug, 'field' => 'slug','terms' => 'design',),), );
@@ -65,7 +65,7 @@ $term_web = get_term_by('name', 'web', 'tipo-de-servico');
 						<li class="lista__item">
 							<figure>
 								<a href="<?php echo get_permalink(); ?>">
-									<div class="lista__bordas wow zoomIn" data-wow-duration="1" data-wow-delay="0">
+									<div class="lista__bordas wow zoomIn" data-wow-duration="0.25" data-wow-delay="0">
 											<!-- Colocar um BG na tag <i> utilizando a classe correspondente ao icone-->
 											<i class="servicos__<?php echo $slug; ?>">
 												<img class="icon" src="<?php echo $icone; ?>" alt="<?php echo the_title(); ?>" />
@@ -81,7 +81,7 @@ $term_web = get_term_by('name', 'web', 'tipo-de-servico');
 						</li>
 						<?php endwhile; ?> 
 						<?php if( is_home() || is_front_page() ): ?>
-						<div class="groupboxes__vermais groupboxes__vermais--servicos wow rotateIn" data-wow-duration="1" data-wow-delay="0">
+						<div class="groupboxes__vermais groupboxes__vermais--servicos wow rotateIn" data-wow-duration="0.25" data-wow-delay="0">
 							<a href="<?php echo home_url(); ?>/servicos/#design" class="btn-plus"></a>
 						</div>
 					<?php endif; ?>
@@ -101,9 +101,9 @@ $term_web = get_term_by('name', 'web', 'tipo-de-servico');
 				 ?>
 				</p>
 			</header>
-			<div class="container">
+			<div class="container-full">
 				<div class="boxes__box-right">
-					<ul class="lista lista--web">
+					<ul class="lista lista--web left-padding--interCol">
 						<?php
 							$tax_slug =  'tipo-de-servico';
 	            			$args = array( 'post_type' => 'servico', 'posts_per_page' => $limit_res, 'order' => 'ASC', 'tax_query' => array(  array( 'taxonomy'  => $tax_slug, 'field' => 'slug','terms' => 'web',),), );
@@ -125,7 +125,7 @@ $term_web = get_term_by('name', 'web', 'tipo-de-servico');
 						<li class="lista__item">
 							<figure>
 								<a href="<?php echo get_permalink(); ?>">
-									<div class="lista__bordas lista__bordas--white wow zoomIn" data-wow-duration="1" data-wow-delay="0">
+									<div class="lista__bordas lista__bordas--white wow zoomIn" data-wow-duration="0.25" data-wow-delay="0">
 											<i class="lista__<?php echo $slug; ?>">
 												<img class="icon" src="<?php echo $icone; ?>" alt="<?php echo the_title(); ?>" />
 												<img class="icon-hover" src="<?php echo $icone_hover; ?>" alt="<?php echo the_title(); ?>" />
@@ -141,13 +141,13 @@ $term_web = get_term_by('name', 'web', 'tipo-de-servico');
 						<?php endwhile; ?>
 					</ul>
 						<?php if( is_home() || is_front_page() ): ?>
-							<div class="groupboxes__vermais groupboxes__vermais--servicos wow rotateIn" data-wow-duration="1" data-wow-delay="0">
+							<div class="groupboxes__vermais groupboxes__vermais--servicos wow rotateIn" data-wow-duration="0.25" data-wow-delay="0">
 								<a href="<?php echo home_url(); ?>/servicos/#web" class="btn-plus"></a>
 							</div>
 						<?php endif; ?>	
 				</div>
 				<div class="boxes__box-left">
-					<header class="header-section header-section--area-design header-section--area-web">
+					<header class="header-section header-section--area-design header-section--area-web right-padding--interCol">
 						<div class="bloco-title bloco-title--right">
 			        		<h2 class="title-web  ">WEB</h2>
 							<p class="subtitle-web">
